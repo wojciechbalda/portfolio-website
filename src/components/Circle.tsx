f9e585721e880963e4bc1d88b267a15c77082f8b
Wojciech Balda <wojtek.balda@gmail.com>
2024-06-04 23:21:42 +0200

    Fix problem related with circle component on mobile devices

--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -6,5 +6,5 @@ type CircleProps = {
 
 export default function Circle({className}: CircleProps)
 {
-    return <div className={cn('rounded-full from-primary to-primary/0 bg-[radial-gradient(circle,_var(--tw-gradient-stops))] aspect-square absolute z-10', className)} />
+    return <div className={cn('rounded-full from-primary to-primary/0 bg-[radial-gradient(circle,_var(--tw-gradient-stops))] aspect-square fixed z-10', className)} />
 }
